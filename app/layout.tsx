fd72d9c38968ecaa2c2415d1657b40efe1f307b7
Masroor Khonkhodzhaev <masroor47@icloud.com>
2023-01-27 23:09:33 -0500

    Add NavBar to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import './globals.css'
 
+import NavBar from './NavBar'
+
 export default function RootLayout({
   children,
 }: {
@@ -12,7 +14,12 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body className='min-h-screen'>
+        <NavBar />
+
+        {children}
+      
+      </body>
     </html>
   )
 }
