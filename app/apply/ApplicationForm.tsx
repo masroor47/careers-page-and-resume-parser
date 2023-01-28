40b56aeb424df3299246bc847612b2f06477ddf2
Masroor Khonkhodzhaev <masroor47@icloud.com>
2023-01-28 00:59:02 -0500

    added styling to apply page and form

--- app/apply/ApplicationForm.tsx
+++ app/apply/ApplicationForm.tsx
@@ -5,26 +5,34 @@
 export default function ApplicationForm() {
 
   return (
-    <form className="">
+    <form className="w-[32rem]">
       <label >Resume</label>
       <input type="file" />
 
-      <div>
-        <label htmlFor="">First Name</label>
-        <input className="border-2 border-blue-300 rounded-lg" type="text" name="" id="" />
+      <div className="flex my-4">
+        <div className="w-48 py-1">
+          <label htmlFor="">Full Name</label>
+        </div>
 
-        <label htmlFor="">Last Name</label>
-        <input className="border-2 border-blue-300 rounded-lg" type="text" name="" id="" />
+          <input className="border-2 border-blue-300 rounded-sm" type="text" name="" id="" />    
       </div>
 
-      <div>
-        <label htmlFor="">Email</label>
-        <input className="border-2 border-blue-300 rounded-lg" type="text" name="" id="" />
+      <div className="flex my-4">
+        <div className="w-48 py-1">   
+          <label htmlFor="">Email</label>
+        </div>
 
-        <label htmlFor="">Phone Number</label>
-        <input className="border-2 border-blue-300 rounded-lg" type="text" name="" id="" />
+          <input className="border-2 border-blue-300 rounded-sm" type="text" name="" id="" />        
       </div>
-      
+
+      <div className="flex my-4">
+        <div className="w-48 py-1">
+          <label htmlFor="">Phone Number</label>
+        </div>
+
+          <input className="border-2 border-blue-300 rounded-sm" type="text" name="" id="" />
+      </div>
+
     </form>
   )
 }
--- app/apply/page.tsx
+++ app/apply/page.tsx
@@ -3,8 +3,10 @@ import ApplicationForm from "./ApplicationForm"
 
 export default function Page() {
   return (
-    <main>
-      <h1 className="text-3xl font-semibold p-4">Fill out the application:</h1>
+    <main className="flex flex-col items-center">
+      <div className="w-[32rem]">
+        <h1 className="text-3xl font-semibold py-4">Fill out the application:</h1>
+      </div>
       
       <ApplicationForm />
     </main>
