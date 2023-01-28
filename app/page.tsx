71fae1a55c1e9a3b3bdb554076ce5a82831d0aea
Masroor Khonkhodzhaev <masroor47@icloud.com>
2023-01-27 23:08:54 -0500

    made intro page with links to apply and dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,91 +1,37 @@
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from './page.module.css'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className='flex flex-col items-center'>
+      
+      <h1 className='text-3xl font-bold m-4'>Welcome to our Careers page!</h1>
+
+      <h1 className='text-2xl font-bold m-4'>Are you:</h1>
+
+      <div className='flex flex-col md:flex-row justify-center'>
+
+        <div className='max-w-sm md:w-1/3 border-black border-2 rounded-lg p-4 flex flex-col items-center justify-between m-4 bg-red-200'>
+          <div className='flex flex-col items-center'>
+            <h1 className='text-xl font-bold p-2'>Student</h1>
+            <p className='text-center font-semibold'>Get rich quick. Go through the leetcode grind, stress because of the interviews, and then get emotional damage as a result of rejection.</p>
+          </div>
+          <Link className='border-black border-2 rounded-lg bg-blue-400 p-1 text-white' href='./apply'>Student</Link>
+        </div>
+
+        <div className='max-w-sm md:w-1/3 border-black border-2 rounded-lg p-4 flex flex-col items-center justify-between m-4 bg-red-200'>
+          <div className='flex flex-col items-center'>
+            <h1 className='text-xl font-bold p-2'>Recruiter</h1>
+            <p className='text-center font-semibold'>Go through an incredible journey of resume review. Check out the new applicants, see if you like any of these... Yikes.</p>
+          </div>
+          <Link className='border-black border-2 rounded-lg bg-blue-400 p-1 text-white' href='./dashboard'>Bro, I work here smh</Link>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      
     </main>
   )
 }
